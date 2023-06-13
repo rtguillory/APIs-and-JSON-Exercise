805f10e384ec08aea15bdbc37635c63efdb5aa47
Rhonda Guillory <guilloryrt.ps@gmail.com>
2023-06-13 16:24:14 -0500

    init commit

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int i = 0; i < 5; i++)
+            {
+                QuoteGenerator.KanyeQuote();
+                QuoteGenerator.RonQuote();
+            }
+
         }
     }
 }
